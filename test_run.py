4842cf87e3d43495f97a5df371d750af4bb19d1e
Djordje Atlialp <djordje.atlialp@gmail.com>
2023-08-16 11:47:56 +0200

    Quack

--- test_run.py
+++ test_run.py
@@ -12,9 +12,6 @@ class TestAdd(unittest.TestCase):
     def test_add_function_with_floats(self):
         self.assertAlmostEqual(add(2.1, 5.2), 7.3)
 
-    def test_add_function_with_floats_wrong(self):
-        self.assertEqual(add(2.1, 5.2), 7.3)
-
 
 if __name__ == "__main__":
     unittest.main()
