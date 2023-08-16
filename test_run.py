eb5765a3a7b3ee570f9f422db0b83d5235cc3b33
Djordje Atlialp <djordje.atlialp@gmail.com>
2023-08-16 11:29:02 +0200

    Add python scripts and gitignore

---
+++ test_run.py
@@ -0,0 +1,17 @@
+import unittest
+
+from run import add
+
+
+class TestAdd(unittest.TestCase):
+
+    def test_add_function(self):
+        self.assertEqual(add(1, 2), 3)
+        self.assertEqual(add(4, 7), 11)
+
+    def test_add_function_with_floats(self):
+        self.assertAlmostEqual(add(2.1, 5.2), 7.3)
+
+
+if __name__ == "__main__":
+    unittest.main()
